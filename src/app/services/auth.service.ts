bee5e0cd013eff4b0ef44d27fa0bb882dc181e44
Donatas <adom.donatas@gmail.com>
2022-08-19 13:10:44 +0300

    Finish user registration createUser method

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,25 +1,44 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/compat/firestore';
 import IUser from '../models/user.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private auth: AngularFireAuth, private db: AngularFirestore) {}
+  private usersCollection: AngularFirestoreCollection<IUser>;
+
+  constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
+    this.usersCollection = this.db.collection('users');
+  }
 
   public async createUser(userData: IUser) {
+    if (!userData.password) {
+      throw new Error('Password not provided!');
+    }
+
     const userCred = await this.auth.createUserWithEmailAndPassword(
       userData.email as string,
       userData.password as string
     );
 
-    await this.db.collection('users').add({
+    if (!userCred.user) {
+      throw new Error("User can't be found");
+    }
+
+    await this.usersCollection.doc(userCred.user.uid).set({
       name: userData.name,
       email: userData.email,
       age: userData.age,
       phoneNumber: userData.phoneNumber,
+    });
+
+    await userCred.user.updateProfile({
+      displayName: userData.name
     });
   }
 }
