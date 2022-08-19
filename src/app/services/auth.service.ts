2de8a03b69f2b8d35ce2a4578b3f61b245f7fe69
Donatas <adom.donatas@gmail.com>
2022-08-19 16:02:08 +0300

    Finish log in functionality

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,16 +4,22 @@ import {
   AngularFirestore,
   AngularFirestoreCollection,
 } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 import IUser from '../models/user.model';
+import { delay, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private usersCollection: AngularFirestoreCollection<IUser>;
+  public isAuthenticated$: Observable<boolean>;
+  public isAuthenticatedWithDelay$: Observable<boolean>;
 
   constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
     this.usersCollection = this.db.collection('users');
+    this.isAuthenticated$ = auth.user.pipe(map((user) => !!user));
+    this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(delay(1000));
   }
 
   public async createUser(userData: IUser) {
@@ -38,7 +44,7 @@ export class AuthService {
     });
 
     await userCred.user.updateProfile({
-      displayName: userData.name
+      displayName: userData.name,
     });
   }
 }
